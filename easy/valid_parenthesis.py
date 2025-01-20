9c5dc7d38cd8706068e2c6b717e9239df43aad1e
192213ira <ira.kulkarni13@gmail.com>
2025-01-20 16:43:11 -0500

    add: valid_parenthesis

---
+++ easy/valid_parenthesis.py
@@ -0,0 +1,51 @@
+class Solution:
+    def isValid_ira(self, s: str) -> bool:
+
+        # create array open
+        # create array close
+        # create dict  mapping poen and close
+        # create empty array stack
+        # iterate thourgh the string
+        # if first element in close => return false
+        # if in open store in stack
+        # if in close, check if stack has the corresponding open, if yes, stack.pop(), else return false
+        # at the end, if stack empty, return true else false
+
+        #O(N)
+        open_list = ['(', '{', '[']
+        close_list = [')', '}', ']']
+
+        pairs = {
+
+            ')' : '(',
+            '}' : '{',
+            ']' : '['
+        }
+
+        stack = []
+
+        if s[0] in close_list:
+            return False
+
+        for parenthesis in s:
+            if parenthesis in open_list:
+                stack.append(parenthesis)
+            elif parenthesis in close_list and not stack:
+                return False
+            elif stack and parenthesis in close_list and pairs[parenthesis] == stack[-1]:
+                stack.pop()
+            else:
+                return False
+        
+        if not stack:
+            return True
+        else:
+            return False
+                
+            
+
+
+
+
+        
+
