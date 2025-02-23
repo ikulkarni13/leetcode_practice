09e000f6bf1aff90a279bdafa5846ffd74f54379
192213ira <ira.kulkarni13@gmail.com>
2025-02-23 14:28:27 -0500

    Pushing solution for contains duplicate

--- easy/contains_duplicate.py
+++ easy/contains_duplicate.py
@@ -14,3 +14,21 @@ class Solution:
                 seen[num] = 1
         
         return False
+    
+    def containsDuplicate_ira(self, nums: List[int]) -> bool:
+
+        #iterate through the list and add elements of the list to a dict
+        #if the element exists in the list return true
+        #if not, add it to the list and make value 1
+        # at the end of the iteration if all the values in the dict are 1 return false
+
+        dict_count = {}
+
+        for num in nums:
+            if num in dict_count:
+                return True
+            else:
+                dict_count[num] = 1
+        
+   
+        return False
