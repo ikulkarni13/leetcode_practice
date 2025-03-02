a85cf96ae8ff9872f1ac3bcbb5def81622931c0c
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-03-01 22:56:01 -0500

    chad's solush

---
+++ easy/find_all_missing_numbers.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findDisappearedNumbers_chad(self, nums: List[int]) -> List[int]:
+        # get the length of nums to get n
+        # iterate in the range from 1 to n + 1
+        # check if each number is in nums
+        # if it's not, add it to result array
+        # return result array
+        
+        # O(n^2)
+        # disappeared = []
+
+        # for num in range(1, len(nums) + 1):
+        #     if num not in nums:
+        #         disappeared.append(num)
+        
+        # return disappeared
+
+        # O(n)
+        nums_set = set(nums)
+        disappeared = []
+
+        for num in range(1, len(nums) + 1):
+            if num not in nums_set:
+                disappeared.append(num)
+
+        return disappeared
