07901d533bffcdff37d64b3fb1621ed6015eb627
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-03-23 20:16:17 -0400

    added elegant solution

--- easy/best_time_to_buy_and_sell_stock.py
+++ easy/best_time_to_buy_and_sell_stock.py
@@ -29,19 +29,29 @@ class Solution:
         sell_day = 1
         max_profit = 0
 
-        while buy_day < (len(prices) - 1):
-            if sell_day == (len(prices) - 1):
-                max_profit = max(max_profit, prices[sell_day] - prices[buy_day])
-                buy_day += 1
-                sell_day = buy_day + 1
-                continue
+        # while buy_day < (len(prices) - 1):
+        #     if sell_day == (len(prices) - 1):
+        #         max_profit = max(max_profit, prices[sell_day] - prices[buy_day])
+        #         buy_day += 1
+        #         sell_day = buy_day + 1
+        #         continue
 
-            if prices[sell_day] - prices[buy_day] <= 0:
-                buy_day += 1
-                sell_day = buy_day + 1
-                continue
+        #     if prices[sell_day] - prices[buy_day] <= 0:
+        #         buy_day += 1
+        #         sell_day = buy_day + 1
+        #         continue
             
+        #     max_profit = max(max_profit, prices[sell_day] - prices[buy_day])
+        #     sell_day += 1
+        
+        # return max_profit
+    
+        while sell_day < len(prices):
+        if prices[sell_day] > prices[buy_day]:
             max_profit = max(max_profit, prices[sell_day] - prices[buy_day])
-            sell_day += 1
+        else:
+            buy_day = sell_day
+        
+        sell_day += 1
         
         return max_profit
