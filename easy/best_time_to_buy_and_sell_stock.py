9a523e4d0ad822bf1631d1bfd7ee6c3606b84791
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-03-23 20:15:16 -0400

    added pseudocode

--- easy/best_time_to_buy_and_sell_stock.py
+++ easy/best_time_to_buy_and_sell_stock.py
@@ -1,4 +1,26 @@
 class Solution:
+    # brute force (get all profits, return greatest), O(n^2)
+    # iterate through prices
+    # iterate through prices from current i to end
+    # if the current profit is greater than max_profit, reassign max_profit
+    # return max profit
+
+    # examples
+    # prices = [7,1,5,3,6,4]
+    # [1, 1, 1...100]
+
+    # edge case
+    # if len(prices) is 1, return 0
+
+    # elegant
+    # initialize buy_day and sell_day and max_profit
+    # while anchor is not equal to prices length minus 1
+    # check current profit
+    # when profit is less than or equal to 0 increment anchor and runner
+    # otherwise we have a valid profit
+    # reassign max_profit
+    # increment runner
+    # if runner is equal to last index, increment anchor and reassign runner
     def maxProfitChad(self, prices: List[int]) -> int:
         if len(prices) == 1:
             return 0
