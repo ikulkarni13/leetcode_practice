35b8a7f16268bb4452da56b37e5458826ec79dd6
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-03-24 22:46:44 -0400

    added chad's solution

---
+++ easy/climbing_stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairsChad(self, n: int) -> int:
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        
+        a, b = 1, 2
+
+        for i in range(3, n + 1):
+            a, b = b, a + b
+
+        return b
